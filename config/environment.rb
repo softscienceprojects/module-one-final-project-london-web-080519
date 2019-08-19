356db88a55f82c918e12a0f995230f7169fce056
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-08-19 17:18:02 +0100

    building out CLI logic of getting username, make or find a wish

--- config/environment.rb
+++ config/environment.rb
@@ -11,3 +11,5 @@ ActiveRecord::Base.establish_connection(
     adapter: 'sqlite3', 
     database: './db/development.db')
 require_all 'lib'
+
+ActiveRecord::Base.logger = nil
--- lib/cli.rb
+++ lib/cli.rb
@@ -6,16 +6,47 @@ require_relative '../config/environment.rb'
 
 
 class CommandLineInterface
-    prompt = TTY::Prompt.new
+
+    def prompt
+        prompt = TTY::Prompt.new
+        prompt
+    end
 
     def greet
-        p "Welcome to WISH, etc etc"
+        p "Welcome!"
+        get_response = prompt.select("What would you like to do?", ['Make a Wish', 'Find a Wish'])
+            #get their response
+            if  get_response == 'Make a Wish'
+                make_a_wish
+            else
+                find_a_wish
+            end
     end
     
+    def make_a_wish
+        p "You have selected Make a Wish. Please enter your username: "
+        the_username = prompt.ask()
+        get_user_by_username(the_username)
 
-    
-    prompt.ask("What is your username?")
-    
+        #create user
+        enter_new_product
+    end
+
+    def find_a_wish
+        p "You have selected Find a Wish. Please enter the username of the Wish you want to find: "
+        get_user_by_username
+    end
+
+    def get_user_by_username()
+        instance_of_user = User.find_or_create_by(username: the_username)
+        instance_of_user
+    end
+
+    def enter_new_product
+        p "You are gonna enter a new product!"
+        new_wish = Wish.create(user_id: get_user_by_username.id)
+        new_wish
+    end
 
 end
 
